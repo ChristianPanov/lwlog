0e2bb040ba62681832a99e69b71cdecec0d11079
ChristianPanov <krissko7209@abv.bg>
2020-04-12 17:29:30 +0300

    Possibly improved file sink output performance

--- lwlog/lwlog/details/file_helper.cpp
+++ lwlog/lwlog/details/file_helper.cpp
@@ -11,46 +11,49 @@ namespace lwlog::details
 	{
 		m_path = path_str;
 		m_open_mode = open_mode;
-		
+
 		if (!std::filesystem::exists(m_path.parent_path()))
 		{
 			std::filesystem::create_directory(m_path.parent_path());
 		}
 
-		m_file.open(m_path, m_open_mode == truncate::on 
-			? std::ios::in | std::ios::out | std::ios::trunc 
-			: std::ios::in | std::ios::out | std::ios::app);
+		m_file = std::fopen(m_path.string().data(), m_open_mode == truncate::off ? "a" : "w");
 	}
 
 	void file_helper::reopen()
 	{
 		if (std::filesystem::exists(m_path))
 		{
-			m_file.open(m_path, m_open_mode == truncate::on
-				? std::ios::in | std::ios::out | std::ios::trunc
-				: std::ios::in | std::ios::out | std::ios::app);
+			m_file = std::fopen(m_path.string().data(), m_open_mode == truncate::off ? "a" : "w");
 		}
 	}
 
 	void file_helper::write(std::string_view message)
 	{
-		if (std::filesystem::exists(m_path))
+		if (m_file != nullptr)
 		{
-			m_file << message;
+			std::setvbuf(m_file, NULL, _IOFBF, 32768);
+			std::fwrite(message.data(), message.size(), 1, m_file);
 		}
 	}
 
-	void file_helper::close()
+	void file_helper::clear()
 	{
-		if (std::filesystem::exists(m_path))
-		{
-			m_file.close();
-		}
+		m_path.clear();
 	}
 
 	bool file_helper::exists()
 	{
 		return std::filesystem::exists(m_path);
+	}
+
+	void file_helper::close()
+	{
+		if (m_file != nullptr)
+		{
+			std::fclose(m_file);
+			m_file = nullptr;
+		}
 	}
 
 	std::size_t file_helper::size() const
--- lwlog/lwlog/details/file_helper.h
+++ lwlog/lwlog/details/file_helper.h
@@ -18,12 +18,13 @@ namespace lwlog::details
 		void open(std::string_view path_str, truncate open_mode = truncate::off);
 		void reopen();
 		void write(std::string_view message);
-		void close();
+		void clear();
 		bool exists();
+		void close();
 		std::size_t size() const;
 
 	private:
-		std::ofstream m_file;
+		std::FILE* m_file{ nullptr };
 		std::filesystem::path m_path;
 		truncate m_open_mode{ truncate::off };
 	};
