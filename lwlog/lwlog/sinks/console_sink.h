d0a4b23b1dc919ec8c3c61fa1f5c3810f248d224
ChristianPanov <krissko7209@abv.bg>
2020-04-12 17:24:50 +0300

    setvbuf full buffering mode - improves output performance tremendously

--- lwlog/lwlog/sinks/console_sink.h
+++ lwlog/lwlog/sinks/console_sink.h
@@ -13,6 +13,7 @@ namespace lwlog::sinks
 
 	inline void console_sink::sink_it(std::string_view message)
 	{
-		fwrite((std::string(message) + "\n").data(), message.size() + 1, 1, stderr);
+		std::setvbuf(stderr, NULL, _IOFBF, 32768);
+		std::fwrite((std::string(message) + "\n").data(), message.size() + 1, 1, stderr);
 	}
 }
