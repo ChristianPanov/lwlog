a53bc905c6509ffb137ac19bb9e23f7c36ba3f9b
Christian Panov <krissko7209@abv.bg>
2020-11-12 03:18:53 +0200

    New format data file

--- lwlog/lwlog/details/datetime.h
+++ lwlog/lwlog/details/datetime.h
@@ -8,7 +8,7 @@
 
 namespace lwlog::details::datetime
 {
-	std::string format_time(std::string format)
+	static std::string format_time(std::string format)
 	{
 		auto now = std::chrono::system_clock::now();
 		auto in_time_t = std::chrono::system_clock::to_time_t(now);
