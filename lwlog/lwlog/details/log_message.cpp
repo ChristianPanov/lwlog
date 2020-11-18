94e9e51f1c57940537d0f0d5d2f47ca7bb78dbf5
Christian Panov <krissko7209@abv.bg>
2020-11-18 20:13:45 +0200

    Possible redundantly repeating code optimization

--- lwlog/lwlog/details/log_message.cpp
+++ lwlog/lwlog/details/log_message.cpp
@@ -6,16 +6,16 @@ namespace lwlog::details
 {
 	log_message::log_message(log_message_t t_log_message)
 		: m_log_message { t_log_message }
-	{}
-
-	std::string log_message::message() const
 	{
 		details::formatter::insert_pattern_data({
 			{"{message}",		"%v", m_log_message.message},
 			{"{log_level}",		"%l", level_to_string(m_log_message.level)},
 			{"{log_level_abr}",	"%L", std::string(1, level_to_string(m_log_message.level)[0])}
 			});
+	}
 
+	std::string log_message::message() const
+	{
 		return details::formatter::format(m_log_message.pattern, m_log_message.should_color);
 	}
 }
