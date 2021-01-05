fb60a4428ffd4a2f6de77b8f5d248179d45970c4
Christian Panov <krissko7209@abv.bg>
2021-01-05 18:50:33 +0200

    Changed functions to static instead of inline for consistency

--- lwlog/lwlog/details/datetime.h
+++ lwlog/lwlog/details/datetime.h
@@ -18,22 +18,22 @@ namespace lwlog::details::datetime
 		return ss.str();
 	}
 
-	inline std::string get_time()					{ return format_time("%T"); }
-	inline std::string get_date()					{ return format_time("%F"); }
-	inline std::string get_date_short()				{ return format_time("%D"); }
-	inline std::string get_second()					{ return format_time("%S"); }
-	inline std::string get_minute()					{ return format_time("%M"); }
-	inline std::string get_hour_24()				{ return format_time("%H"); }
-	inline std::string get_hour_12()				{ return format_time("%I"); }
-	inline std::string get_ampm()					{ return format_time("%p"); }	
-	inline std::string get_12_hour_clock()			{ return format_time("%r"); }
-	inline std::string get_24_hour_clock()			{ return format_time("%R"); }
-	inline std::string get_weekday()				{ return format_time("%A"); }
-	inline std::string get_weekday_abbreviated()	{ return format_time("%a"); }
-	inline std::string get_day()					{ return format_time("%d"); }
-	inline std::string get_month()					{ return format_time("%m"); }
-	inline std::string get_month_name()				{ return format_time("%B"); }
-	inline std::string get_month_name_abbreviated()	{ return format_time("%b"); }
-	inline std::string get_year()					{ return format_time("%Y"); }
-	inline std::string get_year_short()				{ return format_time("%y"); }
+	static std::string get_time()					{ return format_time("%T"); }
+	static std::string get_date()					{ return format_time("%F"); }
+	static std::string get_date_short()				{ return format_time("%D"); }
+	static std::string get_second()					{ return format_time("%S"); }
+	static std::string get_minute()					{ return format_time("%M"); }
+	static std::string get_hour_24()				{ return format_time("%H"); }
+	static std::string get_hour_12()				{ return format_time("%I"); }
+	static std::string get_ampm()					{ return format_time("%p"); }	
+	static std::string get_12_hour_clock()			{ return format_time("%r"); }
+	static std::string get_24_hour_clock()			{ return format_time("%R"); }
+	static std::string get_weekday()				{ return format_time("%A"); }
+	static std::string get_weekday_abbreviated()	{ return format_time("%a"); }
+	static std::string get_day()					{ return format_time("%d"); }
+	static std::string get_month()					{ return format_time("%m"); }
+	static std::string get_month_name()				{ return format_time("%B"); }
+	static std::string get_month_name_abbreviated()	{ return format_time("%b"); }
+	static std::string get_year()					{ return format_time("%Y"); }
+	static std::string get_year_short()				{ return format_time("%y"); }
 }
