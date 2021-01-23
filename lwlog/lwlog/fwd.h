a9b67b0157faafeafb34c5cae3a47741dd958aa8
Christian Panov <krissko7209@abv.bg>
2021-01-23 12:24:10 +0200

    Removed unused aliases from the old formatting architecture

--- lwlog/lwlog/fwd.h
+++ lwlog/lwlog/fwd.h
@@ -4,8 +4,6 @@ namespace std
 {
 	template<class _Ty>
 	class shared_ptr;
-	template<class _Ty, class _Alloc = allocator<_Ty>>
-	class vector;
 }
 
 namespace lwlog
@@ -25,14 +23,8 @@ namespace lwlog::interface
 	class sink;
 }
 
-namespace lwlog::details::formatter_primitives
-{
-	struct attribute_t;
-}
-
 namespace lwlog::primitives
 {
-	using attribute_t = details::formatter_primitives::attribute_t;
 	using sink_ptr = std::shared_ptr<interface::sink>;
 	using logger_ptr = interface::logger*;
 }
