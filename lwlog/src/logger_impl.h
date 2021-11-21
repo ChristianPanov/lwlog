dd03f5c5e2a0dfbaf9d81c2db45e62ad81b77643
Christian Panov <krissko7209@abv.bg>
2021-11-21 21:00:33 +0200

    Changed explicit ctor call to aggregate initialization

--- lwlog/src/logger_impl.h
+++ lwlog/src/logger_impl.h
@@ -81,7 +81,7 @@ namespace lwlog
 		{
 			if (sink->should_sink(t_level))
 			{
-				LogPolicy::log(sink, details::log_message(message, m_name, t_level, meta));
+				LogPolicy::log(sink, { message, m_name, t_level, meta });
 			}
 		}
 	}
