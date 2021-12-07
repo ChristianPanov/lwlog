31986b63b261844597a678460b6fe4264ecf9ec8
Christian Panov <krissko7209@abv.bg>
2021-12-07 03:08:24 +0200

    Much faster file_sink writes with a deferred mechanism, the actual writing happens in the destructor

--- lwlog/src/sinks/file_sink.h
+++ lwlog/src/sinks/file_sink.h
@@ -12,10 +12,31 @@ namespace lwlog::sinks
 	{
 	public:
 		file_sink() = default;
-		explicit file_sink(std::string_view path) : details::file_writer(path) {}
-		void sink_it(const details::log_message& log_msg) override
-		{
-			details::file_writer::write(m_pattern.compile(log_msg));
-		}
+		explicit file_sink(std::string_view path);
+		~file_sink();
+
+	public:
+		void sink_it(const details::log_message& log_msg) override;
+
+	private:
+		std::vector<details::log_message> m_messages;
 	};
+
+	template<typename ThreadingPolicy>
+	file_sink<ThreadingPolicy>::file_sink(std::string_view path)
+		: details::file_writer(path) 
+	{}
+
+	template<typename ThreadingPolicy>
+	file_sink<ThreadingPolicy>::~file_sink()
+	{
+		for (const auto& log_msg : m_messages)
+			details::file_writer::write(m_pattern.compile(log_msg));
+	}
+
+	template<typename ThreadingPolicy>
+	void file_sink<ThreadingPolicy>::sink_it(const details::log_message& log_msg)
+	{
+		m_messages.push_back(log_msg);
+	}
 }
