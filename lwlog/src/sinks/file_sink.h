e31cafba84c7dcdf4a44359fe1897bb97c8d3406
Christian Panov <christianpanov@abv.bg>
2022-11-13 20:09:07 +0200

    The file sink now sinks immediately

--- lwlog/src/sinks/file_sink.h
+++ lwlog/src/sinks/file_sink.h
@@ -16,13 +16,9 @@ namespace lwlog::sinks
 	public:
 		file_sink() = default;
 		explicit file_sink(std::string_view path);
-		~file_sink() override;
 
 	public:
 		void sink_it(const details::record& record) override;
-
-	private:
-		std::vector<details::record> m_messages;
 	};
 
 	template<typename ThreadingPolicy>
@@ -31,15 +27,8 @@ namespace lwlog::sinks
 	{}
 
 	template<typename ThreadingPolicy>
-	file_sink<ThreadingPolicy>::~file_sink()
-	{
-		for (const auto& record : m_messages)
-			details::file_writer::write(sink_t::m_pattern.compile(record));
-	}
-
-	template<typename ThreadingPolicy>
 	void file_sink<ThreadingPolicy>::sink_it(const details::record& record)
 	{
-		m_messages.push_back(record);
+		details::file_writer::write(sink_t::m_pattern.compile(record));
 	}
 }
