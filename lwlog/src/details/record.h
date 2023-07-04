3d8470b405581f562e3b8d0630c6d06f82193aec
Christian Panov <christianpanov@abv.bg>
2023-07-04 18:36:36 +0300

    Moved member initialization out of ctor initializer list

--- lwlog/src/details/record.h
+++ lwlog/src/details/record.h
@@ -10,22 +10,19 @@ namespace lwlog::details
 	struct record
 	{
 		record() = default;
-		record(std::string_view message, level t_level,
-			const source_meta& meta)
+		record(std::string_view message, level t_level, const source_meta& meta)
 			: message{ message }
 			, level{ t_level }
-			, thread_id{ LWLOG_THREAD_ID_FN() }
-			, process_id{ LWLOG_PROCESS_ID_FN() }
 			, meta{ meta }
 		{
 			LWLOG_INIT_TIME_POINT_FN(time_point);
 		}
 
-		std::string_view message;
-		level level;
-		LWLOG_TIME_TYPE time_point{};
-		LWLOG_THREAD_ID_TYPE thread_id;
-		LWLOG_PROCESS_ID_TYPE process_id;
-		source_meta meta;
+		std::string_view		message;
+		level					level;
+		source_meta				meta;
+		LWLOG_TIME_TYPE			time_point	{};
+		LWLOG_THREAD_ID_TYPE	thread_id	{ LWLOG_THREAD_ID_FN() };
+		LWLOG_PROCESS_ID_TYPE	process_id	{ LWLOG_PROCESS_ID_FN() };
 	};
 }
