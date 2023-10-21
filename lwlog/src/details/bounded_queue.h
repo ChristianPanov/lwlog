2c4e878c05be76345fbb01759fe21af38f7689cf
Christian Panov <christianpanov@abv.bg>
2023-10-21 15:22:20 +0300

    Added bounded queue for producer-consumer mechanism

---
+++ lwlog/src/details/bounded_queue.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <atomic>
+#include <vector>
+
+namespace lwlog::details
+{
+    template<std::size_t Capacity, typename T>
+    struct bounded_queue
+    {
+    public:
+        void enqueue(const T& item);
+        T dequeue() const;
+        bool is_empty() const;
+
+    private:
+        std::vector<T> m_storage{ Capacity + 1, T{} };
+        std::atomic<std::size_t> m_write_index{};
+        std::atomic<std::size_t> m_read_index{};
+    };
+}
+
+#include "bounded_queue_impl.h"
---
+++ lwlog/src/details/bounded_queue_impl.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "bounded_queue.h"
+
+namespace lwlog::details
+{
+    template<std::size_t Capacity, typename T>
+    void bounded_queue<Capacity, T>::enqueue(const T& item)
+    {
+        const std::size_t current_write_index{ m_write_index.load() };
+        const std::size_t next_write_index{ (current_write_index + 1) % Capacity };
+
+        if (next_write_index == m_read_index.load()) {}
+
+        m_storage[current_write_index] = item;
+        m_write_index.store(next_write_index);
+    }
+
+    template<std::size_t Capacity, typename T>
+    T bounded_queue<Capacity, T>::dequeue() const
+    {
+        const std::size_t current_read_index{ m_read_index.load() };
+        const std::size_t next_write_index{ (current_read_index + 1) % Capacity };
+
+        if (current_read_index == m_write_index.load()) {}
+
+        m_read_index.store(next_write_index);
+
+        return m_storage[current_read_index];
+    }
+
+    template<std::size_t Capacity, typename T>
+    bool bounded_queue<Capacity, T>::is_empty() const
+    {
+        return m_read_index.load() == m_write_index.load();
+    }
+}
