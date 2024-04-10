079daa5e8c859c3986173d5b4f79c406ff655db2
Christian Panov <christianpanov@abv.bg>
2024-04-10 19:09:13 +0300

    Changed return types

--- lwlog/src/details/os/os.h
+++ lwlog/src/details/os/os.h
@@ -12,24 +12,24 @@
 namespace lwlog::details::os
 {
 	template<typename ThreadIdPolicy>
-	static std::size_t get_thread_id();
+	static std::uint64_t get_thread_id();
 
 	template<typename ProcessIdPolicy>
-	static std::size_t get_process_id();
+	static std::uint64_t get_process_id();
 
 	struct execution_context_base
 	{
 		virtual ~execution_context_base() = default;
 
-		virtual std::size_t thread_id() const = 0;
-		virtual std::size_t process_id() const = 0;
+		virtual std::uint64_t thread_id() const = 0;
+		virtual std::uint64_t process_id() const = 0;
 	};
 
 	template<typename ThreadIdPolicy, typename ProcessIdPolicy>
 	struct execution_context : public execution_context_base
 	{
-		std::size_t thread_id() const override;
-		std::size_t process_id() const override;
+		std::uint64_t thread_id() const override;
+		std::uint64_t process_id() const override;
 	};
 
 	static bool are_ansi_colors_enabled();
--- lwlog/src/details/os/os_impl.h
+++ lwlog/src/details/os/os_impl.h
@@ -3,14 +3,14 @@
 namespace lwlog::details::os
 {
 	template<typename ThreadIdPolicy>
-	std::size_t get_thread_id()
+	std::uint64_t get_thread_id()
     {
-		std::size_t thread_id{};
+		std::uint64_t thread_id{};
 
 		#if defined(_WIN32)
-			thread_id = static_cast<std::size_t>(::GetCurrentThreadId());
+			thread_id = static_cast<std::uint64_t>(::GetCurrentThreadId());
 		#elif defined(__linux__)
-			thread_id = static_cast<std::size_t>(::syscall(SYS_gettid));
+			thread_id = static_cast<std::uint64_t>(::syscall(SYS_gettid));
 		#elif defined(__APPLE__)
 			::pthread_threadid_np(NULL, &thread_id);
 		#else
@@ -21,35 +21,35 @@ namespace lwlog::details::os
 	}
 
 	template<typename ProcessIdPolicy>
-	std::size_t get_process_id()
+	std::uint64_t get_process_id()
 	{
 		#if defined(_WIN32)
-			return static_cast<std::size_t>(::GetCurrentProcessId());
+			return static_cast<std::uint64_t>(::GetCurrentProcessId());
 		#elif defined(__linux__) || defined(__APPLE__)
-			return static_cast<std::size_t>(::getpid());
+			return static_cast<std::uint64_t>(::getpid());
 		#endif
 	}
 
 	template<> 
-	std::size_t get_thread_id<disable_thread_id>() 
+	std::uint64_t get_thread_id<disable_thread_id>()
 	{ 
 		return {}; 
 	}
 
 	template<> 
-	std::size_t get_process_id<disable_process_id>() 
+	std::uint64_t get_process_id<disable_process_id>()
 	{ 
 		return {}; 
 	}
 
 	template<typename ThreadIdPolicy, typename ProcessIdPolicy>
-	std::size_t execution_context<ThreadIdPolicy, ProcessIdPolicy>::thread_id() const
+	std::uint64_t execution_context<ThreadIdPolicy, ProcessIdPolicy>::thread_id() const
 	{ 
 		return get_thread_id<ThreadIdPolicy>();
 	}
 
 	template<typename ThreadIdPolicy, typename ProcessIdPolicy>
-	std::size_t execution_context<ThreadIdPolicy, ProcessIdPolicy>::process_id() const
+	std::uint64_t execution_context<ThreadIdPolicy, ProcessIdPolicy>::process_id() const
 	{ 
 		return get_process_id<ProcessIdPolicy>();
 	}
