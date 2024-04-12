acb3f97288d32b7d20b1341a6cf16560b84a206a
Christian Panov <christianpanov@abv.bg>
2024-04-13 01:14:32 +0300

    Changed static global variables to inline

--- lwlog/src/details/os/datetime_utility.h
+++ lwlog/src/details/os/datetime_utility.h
@@ -14,7 +14,7 @@
 
 namespace lwlog::details::os::datetime
 {
-	static const std::int16_t cached_timezone_offset = []() {
+	inline const std::int16_t cached_timezone_offset = []() {
 		#ifdef _WIN32
 			::TIME_ZONE_INFORMATION tz_info;
 			::GetTimeZoneInformation(&tz_info);
@@ -33,16 +33,16 @@ namespace lwlog::details::os::datetime
 		#endif
 	}();
 
-	static const std::array<const char*, 12> month_name = { "January", "February", "March", "April", "May", 
+	inline const std::array<const char*, 12> month_name = { "January", "February", "March", "April", "May",
 		"June", "July", "August", "September", "October", "November", "December" };
 
-	static const std::array<const char*, 12> month_name_short = { "Jan", "Feb", "Mar", "Apr", "May","Jun", 
+	inline const std::array<const char*, 12> month_name_short = { "Jan", "Feb", "Mar", "Apr", "May","Jun",
 		"Jul", "Aug", "Sept", "Oct", "Nov", "Dec" };
 
-	static const std::array<const char*, 7> weekday_name = { "Monday", "Tuesday", "Wednesday", 
+	inline const std::array<const char*, 7> weekday_name = { "Monday", "Tuesday", "Wednesday",
 		"Thursday","Friday", "Saturday", "Sunday" };
 
-	static const std::array<const char*, 7> weekday_name_short = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+	inline const std::array<const char*, 7> weekday_name_short = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
 
 	template<typename LocalTimePolicy>
 	static std::uint8_t handle_timezone(std::uint8_t hour);
