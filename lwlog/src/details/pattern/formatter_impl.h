588d339d392ea4000973ed39e4140299910afd6a
Christian Panov <christianpanov@abv.bg>
2024-10-11 00:33:23 +0300

    Fixed static analysis issues

--- lwlog/src/details/pattern/formatter_impl.h
+++ lwlog/src/details/pattern/formatter_impl.h
@@ -7,10 +7,10 @@ namespace lwlog::details
 	template<typename T>
 	void formatter::format_attribute(std::string& pattern, const flag_pair& flags, T value)
 	{
-		const std::string_view str_value = [&value]() {
+		const std::string str_value = [&value]() {
 			if constexpr (std::is_arithmetic_v<T>)					return std::to_string(value);
-			else if constexpr (std::is_same_v<T, std::string>)		return value.data();
-			else if constexpr (std::is_same_v<T, std::string_view>)	return value;
+			else if constexpr (std::is_same_v<T, std::string>)		return value;
+			else if constexpr (std::is_same_v<T, std::string_view>)	return value.data();
 			else if constexpr (std::is_same_v<T, const char*>)		return value;
 			else													return "";
 		}();
@@ -30,10 +30,10 @@ namespace lwlog::details
 	template<typename T>
 	void formatter::format_attribute(std::string& pattern, std::string_view flag, T value)
 	{
-		const std::string_view str_value = [&value]() {
+		const std::string str_value = [&value]() {
 			if constexpr (std::is_arithmetic_v<T>)					return std::to_string(value);
-			else if constexpr (std::is_same_v<T, std::string>)		return value.data();
-			else if constexpr (std::is_same_v<T, std::string_view>)	return value;
+			else if constexpr (std::is_same_v<T, std::string>)		return value;
+			else if constexpr (std::is_same_v<T, std::string_view>)	return value.data();
 			else if constexpr (std::is_same_v<T, const char*>)		return value;
 			else													return "";
 		}();
