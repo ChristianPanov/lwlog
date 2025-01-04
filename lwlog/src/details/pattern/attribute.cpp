479d6fb8b0169fd1644fbb7217922aa0868cfa0b
Christian Panov <christianpanov@abv.bg>
2025-01-04 19:40:36 +0200

    #48 Fixed invalid data pointer issue

--- lwlog/src/details/pattern/attribute.cpp
+++ lwlog/src/details/pattern/attribute.cpp
@@ -12,12 +12,12 @@ namespace lwlog::details
 		: flag{ flag }
 		, value{ value }
 	{
-		callback = [value]() -> const char* {
+		callback = [value]() -> std::string {
 			switch (value.index())
 			{
-			case 0:	return std::to_string(std::get<0>(value).get()).data();
-			case 1:	return std::to_string(std::get<1>(value).get()).data();
-			case 2:	return std::to_string(std::get<2>(value).get()).data();
+			case 0:	return std::to_string(std::get<0>(value).get());
+			case 1:	return std::to_string(std::get<1>(value).get());
+			case 2:	return std::to_string(std::get<2>(value).get());
 			case 3:	return std::get<3>(value).get().data();
 			case 4:	return std::get<4>(value).get().data();
 			default: return {};
