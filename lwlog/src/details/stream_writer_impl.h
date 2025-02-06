cf1ac47115cfdd25941218f146726aa9f0920659
Bryan Zimmerman <BryanZim@bazinnovations.com>
2025-02-06 14:17:50 -0500

    Fix clang error

--- lwlog/src/details/stream_writer_impl.h
+++ lwlog/src/details/stream_writer_impl.h
@@ -2,62 +2,72 @@
 
 #include "stream_writer.h"
 
+#include <cstdio>
+
 namespace lwlog::details
 {
-	template<typename FlushPolicy>
-	stream_writer<FlushPolicy>::stream_writer(std::FILE* stream)
-		: m_stream{ stream }
-	{
-		std::setvbuf(m_stream, nullptr, _IOFBF, FlushPolicy::buffer_size);
-	}
+    template<typename FlushPolicy>
+    stream_writer<FlushPolicy>::stream_writer(std::FILE* stream)
+        : m_stream{ stream }
+    {
+        std::setvbuf(m_stream, nullptr, _IOFBF, FlushPolicy::buffer_size);
+    }
 
-	template<typename FlushPolicy>
-	stream_writer<FlushPolicy>::stream_writer(std::string_view path)
-		: m_path{ path }
-	{
-		if (!std::filesystem::exists(m_path.parent_path()))
-		{
-			std::filesystem::create_directory(m_path.parent_path());
-		}
+    template<typename FlushPolicy>
+    stream_writer<FlushPolicy>::stream_writer(std::string_view path)
+        : m_path{ path }
+    {
+        if (!std::filesystem::exists(m_path.parent_path()))
+        {
+            std::filesystem::create_directory(m_path.parent_path());
+        }
 
-		m_stream = std::fopen(m_path.string().data(), "a");
+#if defined(WIN32) and defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
+        m_stream = std::fopen(m_path.string().data(), "a");
 
-		if (m_stream != nullptr)
-		{
-			std::setvbuf(m_stream, nullptr, _IOFBF, FlushPolicy::buffer_size);
-		}
-	}
+#if defined(WIN32) and defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 
-	template<typename FlushPolicy>
-	stream_writer<FlushPolicy>::~stream_writer()
-	{
-		if (!m_path.empty() && m_stream != nullptr)
-		{
-			std::fclose(m_stream);
-			m_stream = nullptr;
-		}
-	}
+        if (m_stream != nullptr)
+        {
+            std::setvbuf(m_stream, nullptr, _IOFBF, FlushPolicy::buffer_size);
+        }
+    }
 
-	template<typename FlushPolicy>
-	void stream_writer<FlushPolicy>::write(std::string_view message) const
-	{
-		if (m_stream != nullptr)
-		{
-			std::fwrite(message.data(), message.size(), 1, m_stream);
-			std::fwrite("\n", 1, 1, m_stream);
-			FlushPolicy::flush(m_stream);
-		}
-	}
+    template<typename FlushPolicy>
+    stream_writer<FlushPolicy>::~stream_writer()
+    {
+        if (!m_path.empty() && m_stream != nullptr)
+        {
+            std::fclose(m_stream);
+            m_stream = nullptr;
+        }
+    }
 
-	template<typename FlushPolicy>
-	std::FILE* stream_writer<FlushPolicy>::handle() const
-	{
-		return m_stream;
-	}
+    template<typename FlushPolicy>
+    void stream_writer<FlushPolicy>::write(std::string_view message) const
+    {
+        if (m_stream != nullptr)
+        {
+            std::fwrite(message.data(), message.size(), 1, m_stream);
+            std::fwrite("\n", 1, 1, m_stream);
+            FlushPolicy::flush(m_stream);
+        }
+    }
 
-	template<typename FlushPolicy>
-	std::filesystem::path& stream_writer<FlushPolicy>::filesystem_path()
-	{
-		return m_path;
-	}
+    template<typename FlushPolicy>
+    std::FILE* stream_writer<FlushPolicy>::handle() const
+    {
+        return m_stream;
+    }
+
+    template<typename FlushPolicy>
+    std::filesystem::path& stream_writer<FlushPolicy>::filesystem_path()
+    {
+        return m_path;
+    }
 }
